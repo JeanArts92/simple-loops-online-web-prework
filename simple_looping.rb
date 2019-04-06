2676addcd12aaba1a65af8565d9383a7943d9b5f
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-06 03:16:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,7 +20,7 @@ def times_iterator(number_of_times)
 def while_iterator(number_of_times)
   number_of_times = 0 
   while number_of_times > 7
-  puts "Welcome to Flatiron School's Web Development Course!"
+  puts ""
   number_of_times += 1 
 end
 end
